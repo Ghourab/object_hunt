2cafb6eab58278c288407f961c1ca27817700f67
HelmyMagdy <helmymagdyattalla@gmail.com>
2022-10-29 16:09:39 +0200

    startScreen

---
+++ lib/screens/startScreen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  // const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+          child: ListView(
+        padding: const EdgeInsets.all(0),
+        children: const [
+          DrawerHeader(
+            child: Text('Menu'),
+            decoration: BoxDecoration(color: Colors.blue),
+          ),
+          ListTile(
+            title: Text('Profile'),
+          ),
+          ListTile(
+            title: Text('Settings'),
+          ),
+        ],
+      )),
+      appBar: AppBar(
+        title: const Text('Welcome to Object Hunt'),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/object_hunt.png'),
+                fit: BoxFit.fill)),
+        child: ListView(padding: const EdgeInsets.all(10), children: [
+          // Image.asset('assets/object_hunt.png'),
+
+          const SizedBox(
+            height: 500,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(onPressed: () {}, child: const Text('Start')),
+              ElevatedButton(onPressed: () {}, child: const Text('Score'))
+            ],
+          )
+        ]),
+      ),
+    );
+  }
+}

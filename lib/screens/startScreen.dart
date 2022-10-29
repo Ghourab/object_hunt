7b918b6a6490a0e6cc79227eb69935149a37b043
HelmyMagdy <helmymagdyattalla@gmail.com>
2022-10-29 16:28:22 +0200

    Update startScreen.dart

--- lib/screens/startScreen.dart
+++ lib/screens/startScreen.dart
@@ -1,51 +1,55 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  // const MyWidget({super.key});
+void main() {
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-          child: ListView(
-        padding: const EdgeInsets.all(0),
-        children: const [
-          DrawerHeader(
-            child: Text('Menu'),
-            decoration: BoxDecoration(color: Colors.blue),
-          ),
-          ListTile(
-            title: Text('Profile'),
-          ),
-          ListTile(
-            title: Text('Settings'),
-          ),
-        ],
-      )),
-      appBar: AppBar(
-        title: const Text('Welcome to Object Hunt'),
-      ),
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/object_hunt.png'),
-                fit: BoxFit.fill)),
-        child: ListView(padding: const EdgeInsets.all(10), children: [
-          // Image.asset('assets/object_hunt.png'),
+    return MaterialApp(
+      home: Scaffold(
+        drawer: Drawer(
+            child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: const [
+            DrawerHeader(
+              child: Text('Menu'),
+              decoration: BoxDecoration(color: Colors.blue),
+            ),
+            ListTile(
+              title: Text('Profile'),
+            ),
+            ListTile(
+              title: Text('Settings'),
+            ),
+          ],
+        )),
+        appBar: AppBar(
+          title: const Text('Welcome to Object Hunt'),
+        ),
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/object_hunt.png'),
+                  fit: BoxFit.fill)),
+          child: ListView(padding: const EdgeInsets.all(10), children: [
+            // Image.asset('assets/object_hunt.png'),
 
-          const SizedBox(
-            height: 500,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(onPressed: () {}, child: const Text('Start')),
-              ElevatedButton(onPressed: () {}, child: const Text('Score'))
-            ],
-          )
-        ]),
+            const SizedBox(
+              height: 500,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(onPressed: () {}, child: const Text('Start')),
+                ElevatedButton(onPressed: () {}, child: const Text('Score'))
+              ],
+            )
+          ]),
+        ),
       ),
     );
   }

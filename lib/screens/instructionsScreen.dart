5fe2c8f5179328b272d6709a88ca00f50e5b4999
hadywk <hadywk@gmail.com>
2022-11-22 13:02:34 +0200

    instructions screen

---
+++ lib/screens/instructionsScreen.dart
@@ -0,0 +1,65 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:intro_slider/intro_slider.dart';
+import 'package:intro_slider/slide_object.dart';
+
+class InstructionScreen extends StatefulWidget {
+  const InstructionScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InstructionScreen> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<InstructionScreen> {
+  List<Slide> slides = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    slides.add(
+      new Slide(
+        widthImage: 300.0,
+        heightImage: 300.0,
+        title: "Getting Started",
+        description: "Use this app in an open space for the best experience.",
+        pathImage: "assets/images/alert_2.png",
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+      ),
+    );
+    slides.add(
+      new Slide(
+        widthImage: 300.0,
+        heightImage: 300.0,
+        title: "Getting Started",
+        description: "Be aware of your surroundings at all times.",
+        pathImage: "assets/images/alert_1.png",
+        backgroundColor:  Color.fromARGB(255, 0, 0, 0),
+      ),
+    );
+    slides.add(
+      new Slide(
+        widthImage: 300.0,
+        heightImage: 300.0,
+        title: "Getting Started",
+        description:
+            "Enable Camera and Disk access to start seeing products in augmented reality. You may accept when asked or go to the settings panel of your device",
+        pathImage: "assets/images/phone.png",
+        backgroundColor:  Color.fromARGB(255, 0, 0, 0),
+      ),
+    );
+  }
+
+  void onDonePress() {
+    log("End of slides");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new IntroSlider(
+      slides: this.slides,
+      onDonePress: this.onDonePress,
+    );
+  }
+}
--- lib/screens/startScreen.dart
+++ lib/screens/startScreen.dart
@@ -7,23 +7,36 @@ import '../screens/hiderScreen.dart';
 
 class StartScreen extends StatelessWidget {
   final User value;
- const StartScreen({Key? key,required this.value}) : super(key: key);
-    void hiderScreen(BuildContext ctx){
-  Navigator.of(ctx).push(MaterialPageRoute(builder: (_){
-    return HiderScreen();
-  },),);
-}
+  const StartScreen({Key? key, required this.value}) : super(key: key);
+  void hiderScreen(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return HiderScreen();
+        },
+      ),
+    );
+  }
 
- void editProfileScreen(BuildContext ctx,User value){
-  Navigator.of(ctx).push(MaterialPageRoute(builder: (_){
-    return EditProfile(value:value);
-  },),);
-}
- void scoreBoardScreen(BuildContext ctx){
-  Navigator.of(ctx).push(MaterialPageRoute(builder: (_){
-    return ScoreBoard();
-  },),);
-}
+  void editProfileScreen(BuildContext ctx, User value) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return EditProfile(value: value);
+        },
+      ),
+    );
+  }
+
+  void scoreBoardScreen(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return ScoreBoard();
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +51,8 @@ class StartScreen extends StatelessWidget {
             decoration: BoxDecoration(color: Colors.blue),
           ),
           ListTile(
-            title: Text('Profile'),onTap: () => editProfileScreen(context,this.value),
+            title: Text('Profile'),
+            onTap: () => editProfileScreen(context, this.value),
           ),
           ListTile(
             title: Text('Settings'),
@@ -54,28 +68,34 @@ class StartScreen extends StatelessWidget {
         width: double.infinity,
         decoration: const BoxDecoration(
             image: DecorationImage(
-            image: AssetImage('assets/images/Logo_final.png'))),
-            child: ListView(padding: const EdgeInsets.all(75), children: [
+                image: AssetImage('assets/images/Logo_final.png'))),
+        child: ListView(padding: const EdgeInsets.all(75), children: [
           // Image.asset('assets/object_hunt.png'),
 
           const SizedBox(
             height: 350,
           ),
           Column(
-           
-           
             children: [
-             
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(onPressed: ()=>hiderScreen(context),
-                
-                 style: ElevatedButton.styleFrom(primary : Colors.teal[200],),
-                 child: const Text('Start', 
-                 style: TextStyle(fontSize: 20, color: Colors.black),),),
+                child: ElevatedButton(
+                  onPressed: () => hiderScreen(context),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.teal[200],
+                  ),
+                  child: const Text(
+                    'Start',
+                    style: TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                ),
               ),
-
-              ElevatedButton(onPressed: () =>scoreBoardScreen(context), child: const Text('Score',style: TextStyle(fontSize: 19.5, color: Colors.black),))
+              ElevatedButton(
+                  onPressed: () => scoreBoardScreen(context),
+                  child: const Text(
+                    'Score',
+                    style: TextStyle(fontSize: 19.5, color: Colors.black),
+                  ))
             ],
           )
         ]),

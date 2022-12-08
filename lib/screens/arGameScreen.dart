84b02741838a2decd303d837673877c2eafcad8f
Ghourab <ahmed1912075@miuegypt.edu.eg>
2022-12-08 17:42:24 +0200

    AR add and remove balls

--- lib/screens/arGameScreen.dart
+++ lib/screens/arGameScreen.dart
@@ -1,12 +1,6 @@
-import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
-import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
-import 'package:ar_flutter_plugin/models/ar_node.dart';
-import 'package:ar_flutter_plugin/widgets/ar_view.dart';
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:flutter/material.dart';
-
-import '../screens/seekerScreen.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 class ArGameScreen extends StatefulWidget {
   
@@ -16,89 +10,114 @@ class ArGameScreen extends StatefulWidget {
 
 class _ArGameScreenState extends State<ArGameScreen> {
   
-  void popScreen(BuildContext ctx){
-  Navigator.of(ctx).pop(ctx);
-}
-
-    void seekerScreen(BuildContext ctx){
-  Navigator.of(ctx).push(MaterialPageRoute(builder: (_){
-    return SeekerScreen();
-  },),);
-}
-// for managing the configurations of session, parameters, and events
-late ARSessionManager arSessionManager;
-
-// manage all node actions
-late ARObjectManager arObjectManager;
-
-// for the position of the object
-ARNode? localObjectNode;
-
-ARNode? webObjectNode;
-
-void onARViewCreated(
-  
-   ARSessionManager arSessionManager,
-   ARObjectManager arObjectManager,
-   ARAnchorManager arAnchorManager,
-   // to get the current location of the device
-   ARLocationManager arLocationManager) {
- // 1
- this.arSessionManager = arSessionManager;
- this.arObjectManager = arObjectManager;
- // 2
- this.arSessionManager.onInitialize(
-       showFeaturePoints: false,
-       showPlanes: true,
-      //  customPlaneTexturePath: "triangle.png",
-       showWorldOrigin: true,
-       handleTaps: false,
-     );
- // 3
- this.arObjectManager.onInitialize();
- 
-} 
-
+   ArCoreController? arCoreController;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-         appBar: AppBar(
-        title: Text('Object Hunt'),
-        backgroundColor: Colors.lightBlue,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: ListView(
-          
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * .8,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(22),
-                child: ARView(
-                  onARViewCreated: onARViewCreated,
-                  permissionPromptDescription: '',
-                ),
-              ),
-            ),
-            
-                Expanded(
-                  child: ElevatedButton(
-                      onPressed: (){
-                        popScreen(context); 
-                        seekerScreen(context);},
-                      child: const Text('End Turn')),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-              
-              ],
-            ),
-          
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello World'),
         ),
-      
+        body: ArCoreView(
+          onArCoreViewCreated: _onArCoreViewCreated,
+          enableTapRecognizer: true,
+          enableUpdateListener:true,
+        ),
+      ),
     );
+  }
+
+  void _onArCoreViewCreated(ArCoreController controller) {
+
+
+    arCoreController = controller;
+    arCoreController!.onNodeTap = (name) => onTapHandler(name);
+    arCoreController!.onPlaneTap = _handleOnPlaneTap;
+    
+  }
+  void _addSphere() {
+    final material = ArCoreMaterial(
+      color: Colors.yellow,
+    );
+    final sphere = ArCoreSphere(
+      materials: [material],
+      radius: 0.1,
+    );
+    final sphereNode = ArCoreNode(
+      shape: sphere,
+      position: vector.Vector3(0, 0, -1.5),
+    );
+    arCoreController?.addArCoreNode(sphereNode);
+  }
+
+   void onTapHandler(String name) {
+  arCoreController?.removeNode(nodeName: '$name');
+    
+  }
+
+void _handleOnPlaneTap(List<ArCoreHitTestResult> hits) {
+      for(int i=0;i<10;i++){
+    print('Ahmed');
+    }
+
+    final hit = hits.first;
+    final material = ArCoreMaterial(
+      color: Colors.red,
+    );
+    final sphere = ArCoreSphere(
+      materials: [material],
+      radius: 0.1,
+    );
+    final sphereNode = ArCoreNode(
+      shape: sphere,
+      position: hit.pose.translation + vector.Vector3(0, 0, 0),
+    );
+    arCoreController?.addArCoreNode(sphereNode);
+
+
+    
+    
+    final moonMaterial = ArCoreMaterial(color: Colors.blue);
+    
+    final moonShape = ArCoreSphere(
+      materials: [moonMaterial],
+      radius: 0.03,
+    );
+    
+    final moon = ArCoreNode(
+      shape: moonShape,
+      position: vector.Vector3(0.2, 0, 0),
+      rotation: vector.Vector4(0, 0, 0, 0),
+    );
+
+    final earthMaterial = ArCoreMaterial(
+        color: Color.fromARGB(120, 66, 134, 244),);
+    
+    final earthShape = ArCoreCube(
+      materials: [earthMaterial],
+      size: vector.Vector3(0.5, 0.5, 0.5),
+    );
+    
+    final earth = ArCoreNode(
+        shape: earthShape,
+        children: [moon],
+        position: hit.pose.translation + vector.Vector3(0.0, 1.0, 0.0),
+        rotation: hit.pose.rotation);
+
+    arCoreController!.addArCoreNodeWithAnchor(earth);
+    arCoreController!.addArCoreNode(earth);
+    arCoreController!.addArCoreNodeWithAnchor(moon);
+    arCoreController!.addArCoreNode(moon);
+    arCoreController?.addArCoreNode(earth);
+    arCoreController?.addArCoreNode(moon);
+  }
+
+  
+
+  @override
+  void dispose() {
+    arCoreController!.dispose();
+    super.dispose();
   }
 }

f4003b5aaf272cbfbba6be1684f43a53a80aa91e
hadywk <hadywk@gmail.com>
2022-12-27 00:33:56 +0200

    Update homeButtons.dart

--- lib/widgets/homeButtons.dart
+++ lib/widgets/homeButtons.dart
@@ -13,20 +13,21 @@ void signUpScreen(BuildContext ctx){
   @override
   Widget build(BuildContext context) {
     return Padding(
-            padding: const EdgeInsets.all(85.0),
+            padding: const EdgeInsets.only(bottom : 60.0),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
                 
-                SizedBox(height:400),
+                // SizedBox(height:400),
                 Padding(
                   padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0,10.0),
                   child: SizedBox(
                     
-                    height: 50,
+                    height: (MediaQuery.of(context).size.height >= 375)?  50: 25,
                     width: 200,
                     child: ElevatedButton(
                       onPressed: ()=>signUpScreen(context),
-                      child: Text('Sign up'),
+                      child: Text('Sign up', style: TextStyle(fontSize: 15),),
                       style: ElevatedButton.styleFrom(
                           shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(30),

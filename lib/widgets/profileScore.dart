754f9415a7ec46097b8e587a45ce82102554f952
hadywk <hadywk@gmail.com>
2022-12-27 00:33:27 +0200

    profile score widget

---
+++ lib/widgets/profileScore.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class ProfileScore extends StatefulWidget {
+  const ProfileScore({ Key? key }) : super(key: key);
+
+  @override
+  State<ProfileScore> createState() => _ProfileScoreState();
+}
+
+class _ProfileScoreState extends State<ProfileScore> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (ctx, constraints){
+      return
+      Container(
+              padding: EdgeInsets.only(top: 40),
+              height: 330,
+              decoration: BoxDecoration(
+                  color: Colors.purple[600],
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(20),
+                    bottomLeft: Radius.circular(20),
+                  )),
+              child: Column(children: [
+                Stack(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          "https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1331&q=80"),
+                      radius: 50,
+                    ),
+                    Positioned(
+                      bottom: 0.0,
+                      right: 0.0,
+                      child: Container(
+                          padding: EdgeInsets.all(4),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle, color: Colors.white),
+                          child: Icon(
+                            Icons.edit,
+                            color: Colors.purpleAccent,
+                          )),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "ahmed sameh",
+                  style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Divider(
+                  thickness: 1,
+                  indent: 20,
+                  endIndent: 20,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        Text("45",
+                            style: TextStyle(
+                              fontSize: 42,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.white.withOpacity(0.9),
+                            )),
+                        Text(
+                          "Level",
+                          style: TextStyle(
+                              fontSize: 19,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "#335",
+                          style: TextStyle(
+                            fontSize: 42,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white.withOpacity(0.9),
+                          ),
+                        ),
+                        Text("Rank",
+                            style: TextStyle(
+                              fontSize: 19,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ))
+                      ],
+                    ),
+                  ],
+                ),
+              ]),
+            );
+            },
+            ); 
+  }
+}

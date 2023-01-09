4c1fb34ce64844c3cb2b188e31450d40bac89898
Mario Shady <60852971+marioshady@users.noreply.github.com>
2023-01-09 21:27:16 +0200

    update api

--- lib/screens/FacebookAPI.dart
+++ lib/screens/FacebookAPI.dart
@@ -14,7 +14,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Codesundar"),
+        title: Text("Facebook API"),
       ),
       body: Container(
         child: _isLoggedIn

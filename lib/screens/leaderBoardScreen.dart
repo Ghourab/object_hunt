fda2b333a53a1b14163fc3dafc767111977724ec
hadywk <hadywk@gmail.com>
2023-01-14 21:37:21 +0200

    layout builder

--- lib/screens/leaderBoardScreen.dart
+++ lib/screens/leaderBoardScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../widgets/gridview_leader.dart';
+import '../widgets/listview_leader.dart';
 import '../widgets/profileScore.dart';
 
 class LeaderBoard extends StatefulWidget {
@@ -13,6 +15,7 @@ class _LeaderBoardState extends State<LeaderBoard> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.teal[200],
         // actions: [
         //   IconButton(onPressed: () {}, icon: Icon(Icons.share)),
         //   IconButton(onPressed: () {}, icon: Icon(Icons.person_add)),
@@ -37,43 +40,19 @@ class _LeaderBoardState extends State<LeaderBoard> {
             ),
             Container(
               //  margin: EdgeInsets.only(bottom: 150),
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 2,
+              width: MediaQuery.of(context).size.width,
               margin: EdgeInsets.all(20),
               child: SizedBox(
                 height: 300,
-                child: ListView.separated(
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Row(
-                          children: [
-                            CircleAvatar(
-                              backgroundImage: NetworkImage(
-                                  "https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=580&q=80"),
-                            ),
-                            SizedBox(
-                              width: 3,
-                            ),
-                            Text("mario")
-                          ],
-                        ),
-                        leading: Text(
-                          "#${index + 1}",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        trailing: Text(
-                            "Rs.${(200000 / (index + 1)).toString().substring(0, 5)}",
-                            style: TextStyle(fontWeight: FontWeight.bold)),
-                      );
-                    },
-                    separatorBuilder: (context, index) => Divider(
-                          thickness: 1,
-                          color: Colors.purple,
-                          indent: 10,
-                          endIndent: 10,
-                        ),
-                    itemCount: 3),
+                child: LayoutBuilder(builder:
+                    (BuildContext context, BoxConstraints constraints) {
+                  if (constraints.maxWidth > 600) {
+                    return GridViewLeader();
+                  } else {
+                    return ListViewLeader();
+                  }
+                }),
               ),
             ),
           ],

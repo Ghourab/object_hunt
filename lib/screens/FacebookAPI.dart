6b233db86b678a931ed752077b7fa09169773279
Ghourab <ahmed1912075@miuegypt.edu.eg>
2023-01-19 18:56:31 +0200

    Revert "Updated requirement SDK for Facebook API"
    This reverts commit 35a236181b555dfaa994bd479ce3745696629a15.

--- lib/screens/FacebookAPI.dart
+++ lib/screens/FacebookAPI.dart
@@ -1,58 +1,58 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
+// class HomePage extends StatefulWidget {
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
 
-class _HomePageState extends State<HomePage> {
-  bool _isLoggedIn = false;
-  Map _userObj = {};
+// class _HomePageState extends State<HomePage> {
+//   bool _isLoggedIn = false;
+//   Map _userObj = {};
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Login with Facebook"),
-      ),
-      body: Container(
-        child: _isLoggedIn
-            ? Column(
-                children: [
-                  Image.network(_userObj["picture"]["data"]["url"]),
-                  Text(_userObj["name"]),
-                  Text(_userObj["email"]),
-                  TextButton(
-                      onPressed: () {
-                        FacebookAuth.instance.logOut().then((value) {
-                          setState(() {
-                            _isLoggedIn = false;
-                            _userObj = {};
-                          });
-                        });
-                      },
-                      child: Text("Logout"))
-                ],
-              )
-            : Center(
-                child: ElevatedButton(
-                  child: Text("Login with Facebook"),
-                  onPressed: () async {
-                    FacebookAuth.instance.login(
-                        permissions: ["public_profile", "email"]).then((value) {
-                      FacebookAuth.instance.getUserData().then((userData) {
-                        setState(() {
-                          _isLoggedIn = true;
-                          _userObj = userData;
-                        });
-                      });
-                    });
-                  },
-                ),
-              ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Login with Facebook"),
+//       ),
+//       body: Container(
+//         child: _isLoggedIn
+//             ? Column(
+//                 children: [
+//                   Image.network(_userObj["picture"]["data"]["url"]),
+//                   Text(_userObj["name"]),
+//                   Text(_userObj["email"]),
+//                   TextButton(
+//                       onPressed: () {
+//                         FacebookAuth.instance.logOut().then((value) {
+//                           setState(() {
+//                             _isLoggedIn = false;
+//                             _userObj = {};
+//                           });
+//                         });
+//                       },
+//                       child: Text("Logout"))
+//                 ],
+//               )
+//             : Center(
+//                 child: ElevatedButton(
+//                   child: Text("Login with Facebook"),
+//                   onPressed: () async {
+//                     FacebookAuth.instance.login(
+//                         permissions: ["public_profile", "email"]).then((value) {
+//                       FacebookAuth.instance.getUserData().then((userData) {
+//                         setState(() {
+//                           _isLoggedIn = true;
+//                           _userObj = userData;
+//                         });
+//                       });
+//                     });
+//                   },
+//                 ),
+//               ),
+//       ),
+//     );
+//   }
+// }
